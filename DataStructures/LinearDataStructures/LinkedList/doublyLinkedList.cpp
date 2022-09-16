b60d4fce518e2c374ae0a2b64be0a7bbd26600ea
Atharva Dongare <agdongare4@gmail.com>
2022-09-15 20:47:55 -0500

    Updated doubly linked list
    Updated doubly linked list code for displaying data backwards and forwards

--- DataStructures/LinearDataStructures/LinkedList/doublyLinkedList.cpp
+++ DataStructures/LinearDataStructures/LinkedList/doublyLinkedList.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
 using namespace std;
 
+struct node
+{
+    int data;
+    struct node *forward;
+    struct node *backward;
+};
+
+void displayNodesBackward(struct node *tail)
+{
+    while (tail)
+    {
+        cout << tail->data << endl;
+        tail = tail->backward;
+    }
+}
+
+void displayNodesForward(struct node *head)
+{
+    while (head)
+    {
+        cout << head->data << endl;
+        head = head->forward;
+    }
+}
+
 int main()
 {
-    cout << "Hello world !" << endl;
+    struct node *head = NULL;
+
+    head = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 1;
+    head->forward = NULL;
+    head->backward = NULL;
+
+    struct node *first;
+    struct node *second;
+    struct node *third;
+
+    first = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+
+    head->backward = NULL;
+
+    head->forward = first;
+    first->backward = head;
+
+    first->forward = second;
+    first->data = 2;
+    second->backward = first;
+
+    second->forward = third;
+    second->data = 3;
+    third->backward = second;
+
+    third->forward = NULL;
+    third->data = 4;
+
+    displayNodesForward(head);
+    cout << endl;
+    displayNodesBackward(third);
 }
