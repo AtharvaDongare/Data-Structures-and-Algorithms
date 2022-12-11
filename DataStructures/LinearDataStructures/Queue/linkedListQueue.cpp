da1a0773c3cc9ac579cae5998869c9b5aa735698
Atharva Dongare <agdongare4@gmail.com>
2022-12-11 15:20:08 -0600

    adding the codes for implementation of Queue using linked List

--- DataStructures/LinearDataStructures/Queue/linkedListQueue.cpp
+++ DataStructures/LinearDataStructures/Queue/linkedListQueue.cpp
@@ -2,17 +2,75 @@
 using namespace std;
 
 class Queue{
+	private:
+	struct node{
+		int data;
+		struct node * forward;	
+	};
+	struct node * head;
+	struct node * tail;
+
 	public:
-	void helloWorld();
-	
+	void createQueue (int element);
+	void putElement (int element);
+	int getElement ();
+	int delElement ();		
 };
 
 
-void Queue::helloWorld(){
-	cout << "Hello WOrld!" << endl;
+void Queue::createQueue (int element){
+	head = (struct node*) malloc (sizeof(struct node));
+	tail = NULL;
+	
+	head -> data = element;
+	head -> forward = tail;
+
+	cout << "The Queue has been created!" << endl;
+
+	return;
 }
+
+void Queue::putElement(int element){
+	struct node * temp = (struct node*) malloc (sizeof(struct node));
+	temp -> data = element;
+	temp ->forward = NULL;
+	if (!head -> forward){
+		head -> forward = temp;
+		tail = temp;
+		return;
+	}
+	
+	tail -> forward = temp;
+	tail = temp;
+}
+
+int Queue::getElement(){
+	return head -> data;
+}
+
+int Queue::delElement(){
+	int temp = head -> data;
+	head = head -> forward;
+	return temp;
+}
+
 
 int main (){
 	Queue q1;
-	q1.helloWorld();
+	
+	q1.createQueue(1);
+	q1.putElement(2);
+	q1.putElement(4);
+	q1.putElement(5);
+	q1.putElement(6);
+
+	cout << q1.getElement() << endl;
+
+	cout << q1.delElement() << endl;
+	cout << q1.delElement() << endl;
+	cout << q1.delElement() << endl;
+	cout << q1.delElement() << endl;
+	cout << q1.delElement() << endl;
+
+
 }
