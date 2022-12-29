295b7ec86cd3d848371d2d683ce50e931be02048
Atharva Dongare <agdongare4@gmail.com>
2022-12-28 20:54:20 -0600

    Adding information about vectors

--- DataStructures/CppSTL/vectorsCpp.cpp
+++ DataStructures/CppSTL/vectorsCpp.cpp
@@ -3,8 +3,10 @@
 
 using namespace std; 
 
+
 int main () {
 	vector<int> v1 , v2;
+	cout << endl << "Vector are an implementation of array in CPP STL. Vectors are more powerful than normal arrays, since they provide dynamic memory allocation. \nPush and pop operations with requried adjustments in the array elements. We have discussed these default functions below with examples. " << endl;
 	for (int i =0 ; i < 10; i++ ){
 		v1.insert( v1.begin() , i);
 	} 
@@ -23,6 +25,8 @@ int main () {
 		cout << v2[i] << " ";
 	}
 
+	cout << endl << endl << "Displaying the sizes and memory availibiity for the vector" << endl;
+
 	cout << endl << endl;
 	cout << "size() gives the total number of elements in the vector" << endl;
 	cout << "Size of the Vector1: " << v1.size() << endl;
@@ -32,7 +36,7 @@ int main () {
 
 	cout << "max_size() gives the maximum number of elements the vector can hold" << endl;
 	cout << "Max size of the Vector1: " <<v1.max_size() << endl;
-	cout << "Max size of the Vector2: " << v1.max_size() << endl;
+	cout << "Max size of the Vector2: " << v2.max_size() << endl;
 
 	cout << endl << endl;
 
@@ -59,11 +63,11 @@ int main () {
 	v1.erase(v1.end()) ;
 	
 
+	cout << "Displaying the element from queue" << endl << endl;
+	
 	cout << "Front of the queue : " << v1.front() << endl << endl ;
 	
 	cout << "Back of the queue : " << v1.back() << endl << endl ;
-	
-	cout << "Displaying the element from queue" << endl << endl;
 	
 	cout << "at : v1.at(4) -> " << v1.at(4) << endl;
 
