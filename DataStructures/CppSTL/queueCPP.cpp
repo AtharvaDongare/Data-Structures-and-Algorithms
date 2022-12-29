533da582ef68cb6d615a6cd31a9cee434099ef14
Atharva Dongare <agdongare4@gmail.com>
2022-12-28 22:10:12 -0600

    Adding the code for STL implementaion of queues

--- DataStructures/CppSTL/queueCPP.cpp
+++ DataStructures/CppSTL/queueCPP.cpp
@@ -3,8 +3,21 @@
 
 using namespace std;
 
+void displayQueue(queue<int> q1){
+	queue<int> temp = q1;
+
+	while(!temp.empty()){  // Checking whether the queue is empty or not
+		cout << temp.front() << " ";
+		temp.pop(); // Popping the top element from the queue
+	}
+	cout << endl << endl;
+}
 
 int main () {
-	cout << "Hello World!" << endl;
+	queue<int> q1;
+	for (int i = 0 ; i < 10 ; i++ )
+		q1.push(i*4); // Adding the elements into the queue
 
+	displayQueue(q1);
+	
 }  
