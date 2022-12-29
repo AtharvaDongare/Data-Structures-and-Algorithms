94740461edd26c7a508368b92502c48dac6d9221
Atharva Dongare <agdongare4@gmail.com>
2022-12-29 00:46:55 -0600

    Added files for STL implementation of Stack

---
+++ DataStructures/CppSTL/stackCpp.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+void displayStack(stack <int> s1){
+	stack <int> temp = s1;
+	
+	while (!temp.empty()) { // Checking whether the stack is empty or not
+		cout << temp.top() << " "; // Printing the top element from stack
+		temp.pop(); // Deleting the top element
+	}
+	cout << endl ;
+
+}
+
+int main(){
+	stack <int> s1;
+
+	for (int i =0 ; i < 10 ; i++ )
+		s1.push(i);
+	
+	cout << "Display the top element of stack : " << s1.top() << endl;; // Showing the top element from the stack
+	cout << "Deleting the top element from the stack ";
+	s1.pop();
+	displayStack(s1);
+	cout << endl;
+	
+}
