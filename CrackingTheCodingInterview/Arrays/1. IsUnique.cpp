db31a414d263eaabd1eb865321f56dc7132e41bd
AtharvaDongare <agdongare4@gmail.com>
2024-01-10 02:27:45 +0530

    Added folders for cracking the coding interview problems sovled

---
+++ CrackingTheCodingInterview/Arrays/1. IsUnique.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Is Unique : Implement an algorithm to determine if a string has all unique characters.What if you cannot use additional data structures?
+
+int main()
+{
+    vector<int> vec1;
+    int _size;
+
+    cin >> _size;
+    for (int i = 0; i < _size; i++)
+    {
+        cin >> vec1[i];
+    }
+
+    // sort(vec1) ;
+
+    vec1.sort();
+    for (int i = 0; i < _size; i++)
+    {
+    }
+}
