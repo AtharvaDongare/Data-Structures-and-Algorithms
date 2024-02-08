4c9a77de8e24f1f75a94229bef12375ae1d6d8bd
AtharvaDongare <agdongare4@gmail.com>
2024-02-08 16:55:14 -0600

    Wrote some temp code in this directory

---
+++ CrackingTheCodingInterview/LinkedLists/1.  RemoveDups.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class RemoveDups
+{
+private:
+    list<int> ll1;
+
+public:
+    void updateList();
+
+    void removeDupbs();
+};
+
+void RemoveDups ::updateList()
+{
+    for (int i = 0 ; i < 10 ; i++ )
+}
+
+int main()
+{
+    cout << "Hello World" << endl;
+}
