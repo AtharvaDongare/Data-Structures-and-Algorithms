f6b342191f30ebe8cea97a45ce7cd2aff7fa1ca6
Atharva Dongare <55730677+AtharvaDongare@users.noreply.github.com>
2024-09-14 19:14:35 -0500

    Delete CrackingTheCodingInterview directory

--- CrackingTheCodingInterview/LinkedLists/7. listIntersection.cpp
+++
@@ -1,4 +0,0 @@
-// Impelement a hash map consisting of all the elements from the list1
-// once we have the hashmap then we can iterate over the second list
-// while we are iterating over the second list we will keep looking into the hashmap
-// the first occurance of node which is there on both sides will be returned since that is the value
--- CrackingTheCodingInterview/LinkedLists/8. listLoopDetection.cpp
+++
@@ -1,3 +0,0 @@
-// use the tortoise and hare approach
-// one fast pointer which skips over one node and the second slow pointer traveling at normal speed
-// if they meet then the list has a loop within itself, if not then they dont have a loop
